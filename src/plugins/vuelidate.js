0d8a6c5773beb727acb85781da104105d69ed873
Asep Wahyudi <asepwahyudi1501@gmail.com>
2026-01-07 01:54:32 +0700

    feat: add vuelidate plugin

---
+++ src/plugins/vuelidate.js
@@ -0,0 +1,5 @@
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength, helpers } from '@vuelidate/validators'
+
+export { useVuelidate, required, email, minLength, helpers }
+
