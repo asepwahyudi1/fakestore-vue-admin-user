d71d3fb29738e25cd88d07efe9d6dc9591bfd281
Asep Wahyudi <asepwahyudi1501@gmail.com>
2026-01-07 01:53:42 +0700

    feat: setup app with pinia router theme and i18n in main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Icon } from '@iconify/vue'
 
 import App from './App.vue'
 import router from './router'
 import './styles/index.css'
+import { useThemeStore } from './stores/theme'
+import { useI18nStore } from './stores/i18n'
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.component('Icon', Icon)
+
+const themeStore = useThemeStore()
+themeStore.initTheme()
+
+const i18nStore = useI18nStore()
+document.documentElement.lang = i18nStore.locale
 
 app.mount('#app')
