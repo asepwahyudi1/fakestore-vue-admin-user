468bd917b5b4c4ed41ac8aacab0a78f16d6962f9
Asep Wahyudi <asepwahyudi1501@gmail.com>
2026-01-07 01:54:33 +0700

    feat: add authentication service

---
+++ src/services/authService.js
@@ -0,0 +1,46 @@
+import { axios } from '@/plugins/axios'
+import { API_ENDPOINTS } from '@/constants'
+import { getFromStorage, setToStorage, removeFromStorage } from '@/utils'
+import { STORAGE_KEYS } from '@/constants'
+import { userService } from './userService'
+
+export const authService = {
+  async login(username, password) {
+    const response = await axios.post(API_ENDPOINTS.AUTH.LOGIN, {
+      username,
+      password,
+    })
+
+    if (response.token) {
+      setToStorage(STORAGE_KEYS.AUTH_TOKEN, response.token)
+      setToStorage(STORAGE_KEYS.USERNAME, username)
+
+      try {
+        const userData = await userService.getUserByUsername(username)
+        if (userData) {
+          setToStorage(STORAGE_KEYS.USER_DATA, userData)
+          return { ...response, user: userData }
+        }
+      } catch (error) {
+        console.error('Failed to fetch user data:', error)
+      }
+    }
+
+    return response
+  },
+
+  logout() {
+    removeFromStorage(STORAGE_KEYS.AUTH_TOKEN)
+    removeFromStorage(STORAGE_KEYS.USER_DATA)
+    removeFromStorage(STORAGE_KEYS.USERNAME)
+  },
+
+  getCurrentUser() {
+    return getFromStorage(STORAGE_KEYS.USER_DATA, null)
+  },
+
+  isAuthenticated() {
+    const token = getFromStorage(STORAGE_KEYS.AUTH_TOKEN, null)
+    return !!token
+  },
+}
